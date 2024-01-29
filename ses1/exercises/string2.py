5c4f82e546cf274ad81794ba73b0be5533512089
F4MES <tobias.schwartzlose@gmail.com>
2024-01-29 10:25:22 +0000

    done

--- ses1/exercises/string2.py
+++ ses1/exercises/string2.py
@@ -12,6 +12,11 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 def verbing(s):
+  if len(s) >= 3:
+    if s[-3:] == 'ing':
+      s = s + 'ly'
+    else:
+      s = s + 'ing'
   return  s
 
 
@@ -24,7 +29,11 @@ def verbing(s):
 # So 'This dinner is not that bad!' yields:
 # This dinner is good!
 def not_bad(s):
-  return s
+    not_index = s.find('not')
+    bad_index = s.find('bad')
+    if not_index < bad_index:
+        s = s[:not_index] + 'good' + s[bad_index+3:]
+    return s
   
 
 
@@ -36,7 +45,9 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
-  return ''  
+    a_mid = (len(a) + 1) // 2
+    b_mid = (len(b) + 1) // 2
+    return a[:a_mid] + b[:b_mid] + a[a_mid:] + b[b_mid:]
 
 
 # Simple provided test() function used in main() to print
